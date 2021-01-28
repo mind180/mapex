57fe9124b23b717a3da73805cc9ef30b18e38e8d
billjoynf <45283458+billjoynf@users.noreply.github.com>
2021-01-28 17:38:57 +0200

    fix: rename to more specific name

--- roadmapex/src/components/ui/dashboard/Dashboard.js
+++ roadmapex/src/components/ui/dashboard/Dashboard.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
+import AddBoard from "../add-board/AddBoard";
 import BoardPin from '../board-pin/BoardPin';
-import './Boards.css';
+import './Dashboard.css';
 
-export default function Boards() {
+export default function Dashboard() {
   return(
     <div className="boards-holder">
+      <AddBoard/>
       {[1, 2, 3, 4, 5, 7, 8, 9, 10, 11, 12, 13, 14, 15].map(num =>
-        <BoardPin />
+        <BoardPin/>
       )}
     </div>
   );
