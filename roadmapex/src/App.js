f5a13eca5b9f5becfa2cd16fceb8784ff365bab8
billjoynf <45283458+billjoynf@users.noreply.github.com>
2021-01-29 16:23:50 +0200

    add: route to board

--- roadmapex/src/App.js
+++ roadmapex/src/App.js
@@ -5,6 +5,7 @@ import CanvasStateProvider from './components/wrappers/CanvasStateProvider.js';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import ProfileHeader from './components/ui/profile-header/ProfileHeader.js';
 import Dashboard from "./components/ui/dashboard/Dashboard";
+import Board from "./components/ui/board/Board";
 
 function App() {
   return (
@@ -19,8 +20,11 @@ function App() {
             <ProfileHeader />
             <Dashboard />
           </Route>
+          <Route path="/canvas/:canvasId">
+            <Board/>
+          </Route>
           <Route path="/">
-            <CanvasStateProvider />
+            <CanvasStateProvider canvasId='5494c80e-c617-4120-b0a5-0a99be0df8ca'  />
             <section className="comments">
               Comments section
             </section>
