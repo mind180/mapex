8b1b6d0f0e7adb2c8fef96bd0b5bd5e4310e5ac8
billjoynf <45283458+billjoynf@users.noreply.github.com>
2021-01-29 13:09:57 +0200

    feat: create canvas via click

--- roadmapex/src/components/ui/add-board/AddBoard.js
+++ roadmapex/src/components/ui/add-board/AddBoard.js
@@ -1,11 +1,23 @@
 import React, {useState} from 'react';
 import './AddBoard.css';
+import { processEntity } from "../../../api/api";
 
 export default function AddBoard() {
+  const newCanvas = {
+    name: 'Untitled',
+    description: ''
+  };
+
+  const createCanvas = () => {
+    processEntity('POST', '/canvas', newCanvas)
+        .then(canvas => console.log(canvas))
+        .catch(error => console.error(error));
+  };
+
   return (
     <div
       className='add-board'
-      onClick={e => console.log('on click')}
+      onClick={createCanvas}
     >
       <div className="add-board__shadow">
         <div className="add-board__plus">+</div>
