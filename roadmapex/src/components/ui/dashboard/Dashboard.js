be266b0dfe7fcae4fcf0cbc6aebaefafdb2fc872
billjoynf <45283458+billjoynf@users.noreply.github.com>
2021-01-30 10:53:01 +0200

    add: fetch boards

--- roadmapex/src/components/ui/dashboard/Dashboard.js
+++ roadmapex/src/components/ui/dashboard/Dashboard.js
@@ -1,14 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import AddBoard from "../add-board/AddBoard";
 import BoardPin from '../board-pin/BoardPin';
+import { processEntity } from '../../../api/api';
 import './Dashboard.css';
 
 export default function Dashboard() {
+  const [boards, setBoards] = useState([]);
+
+  useEffect(() => {
+    let isMounted = true;
+
+    processEntity('GET', '/canvas')
+        .then(response => response.json())
+        .then(boards => {
+          if (isMounted) setBoards(boards)
+        })
+        .catch(error => console.log(error));
+
+    return () => { isMounted = false };
+  });
+
   return(
     <div className="boards-holder">
       <AddBoard/>
-      {[1, 2, 3, 4, 5, 7, 8, 9, 10, 11, 12, 13, 14, 15].map(num =>
-        <BoardPin/>
+      {boards.map(board =>
+        <BoardPin
+          key={board.id}
+          title={board.name}
+          description={board.description}
+        />
       )}
     </div>
   );
