4a10c8395448e5c1d555c018f787d2a73354fa0e
billjoynf <45283458+billjoynf@users.noreply.github.com>
2021-02-06 18:09:30 +0200

    fix: stop event propagation after modal

--- roadmapex/src/components/ui/modal/fragments/modal-content/ModalContent.js
+++ roadmapex/src/components/ui/modal/fragments/modal-content/ModalContent.js
@@ -2,8 +2,12 @@ import React from 'react';
 import './ModalContent.css';
 
 export default function ModalContent({ children }) {
+  const stopEventPropagation = (e) => e.stopPropagation();
+
   return (
-      <div className='modal-content'>
+      <div className='modal-content'
+           onClick={stopEventPropagation}
+      >
         {children}
       </div>
   );
