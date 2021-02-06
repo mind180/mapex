c8e8667de2f380739eea19f042d1708d53cf6cb8
billjoynf <45283458+billjoynf@users.noreply.github.com>
2021-02-06 18:08:26 +0200

    fix: add more parameters and fix names

--- roadmapex/src/components/ui/modal/compiled-modals/EditCanvas.js
+++ roadmapex/src/components/ui/modal/compiled-modals/EditCanvas.js
@@ -2,33 +2,36 @@ import React, {useRef} from 'react';
 import './EditCanvas.css';
 import Modal from '../Modal';
 
-export default function EditCanvas({ onCancel, onCreate}) {
+export default function EditCanvas(props) {
+  const {title = '', description = '', okButtonName = 'Save', onCancel, onOk} = props;
   const titleElement = useRef();
   const descriptionElement = useRef();
 
-  const handleClickCreate = () => {
+  const handleClickOk = () => {
     const name = titleElement.current.value;
     const description = descriptionElement.current.value;
 
-    onCreate({ name, description});
+    if (onOk) {
+      onOk({name, description});
+    }
   };
 
   return (
       <Modal.Content>
         <Modal.Body>
           <div className="modal-body-item">
-            <input ref={titleElement} placeholder='Title' type="text"/>
+            <input ref={titleElement} defaultValue={title} placeholder='Title' type="text"/>
           </div>
           <div className="modal-body-item">
-            <textarea ref={descriptionElement} placeholder='Description'/>
+            <textarea ref={descriptionElement} defaultValue={description} placeholder='Description'/>
           </div>
         </Modal.Body>
         <Modal.Footer>
           <Modal.PrimaryButton action={onCancel}>
               Cancel
           </Modal.PrimaryButton>
-          <Modal.PrimaryButton action={handleClickCreate}>
-            Create
+          <Modal.PrimaryButton action={handleClickOk}>
+            {okButtonName}
           </Modal.PrimaryButton>
         </Modal.Footer>
       </Modal.Content>
