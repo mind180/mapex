b6fdb88cb1c2066948cd79854a0c46a769ca72eb
billjoynf <45283458+billjoynf@users.noreply.github.com>
2021-02-09 14:55:04 +0200

    add: methods

--- src/main/java/com/roadmapex/roadmapex/repository/canvas/UserRepository.java
+++ src/main/java/com/roadmapex/roadmapex/repository/canvas/UserRepository.java
@@ -3,9 +3,12 @@ package com.roadmapex.roadmapex.repository.canvas;
 import com.roadmapex.roadmapex.model.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends CrudRepository<User, UUID> {
 
-  User getUserByUsername(String username);
+  Optional<User> getUserByUsername(String username);
+
+  Optional<User> getUserById(UUID id);
 }
