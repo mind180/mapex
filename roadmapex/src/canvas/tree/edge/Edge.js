a4573cbee8fe045e39209baa676ec1562e7b60e1
billjoynf <45283458+billjoynf@users.noreply.github.com>
2021-02-24 14:05:49 +0200

    implement: two type of lines

--- roadmapex/src/canvas/tree/edge/Edge.js
+++ roadmapex/src/canvas/tree/edge/Edge.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { pathForm } from './lib/pathForm';
+import { position } from './lib/position';
 
 export default class Edge extends React.Component {
   render() {
     if (!this.props.isShown) return null;
 
-    const directionName = this.getDirectionName(this.props.from, this.props.to);
-    const positionStyle = this.positionStyle[directionName](this.props.from, this.props.to);
-    const d = this.drawCommand[directionName](positionStyle.height, positionStyle.width);
+    const {from, to, width = 1, type = 'straight'} = this.props;
 
+    const directionName = this.getDirectionName(from, to);
+
+    const positionStyle = position[directionName](from, to, width);
     const style = {
       position: 'absolute',
       top: positionStyle.y,
@@ -15,12 +18,14 @@ export default class Edge extends React.Component {
       height: positionStyle.height,
       width: positionStyle.width
     };
-    
+
+    const d = pathForm[type][directionName](positionStyle.height, positionStyle.width, width);
+
     return (
       <svg xmlns="http://www.w3.org/2000/svg" style={style}>
-        <path d={d} stroke="black" fill="transparent"></path>
+        <path d={d} stroke="black" strokeLinecap="round" fill="transparent" strokeWidth={width}/>
       </svg>
-   );
+    );
   }
 
   getDirectionName(from ,to) {
@@ -48,66 +53,4 @@ export default class Edge extends React.Component {
   isTop(from, to) {
     return from.y > to.y;
   }
-
-  drawCommand = {
-    BottomRight: function(height, width) {
-      return "M0 0 " + 
-      "Q " + width / 3.5 + " 0 " +
-      width / 2 + " " + height / 2 +
-      " T " + width + " " + height;
-    },
-    BottomLeft: function(height, width) {
-      return "M" + width + " 0 " + 
-      "Q " + width / 1.5 + " 0 " +
-      width / 2 + " " + height / 2 +
-      " T 0 " + height;
-    },
-    TopLeft: function(height, width) {
-      return "M0 0 " + 
-      "Q " + width / 3.5 + " 0 " +
-      width / 2 + " " + height / 2 +
-      " T " + width + " " + height;
-    },
-    TopRight: function(height, width) {
-      return "M0 " + height + " " + 
-      "Q " + width / 3.5 + " " + height + " " +
-      width / 2 + " " + height / 2 +
-      " T " + width + " " + 0;
-    }
-  };
-
-  positionStyle = {
-    BottomRight: function(from, to) {
-      return {
-        x: from.x, 
-        y: from.y,
-        width: Math.floor(to.x - from.x) ? to.x - from.x : 1,
-        height: Math.floor(to.y - from.y) ? to.y - from.y : 1
-      };
-    },
-    BottomLeft: function(from, to) {
-      return {
-        x: to.x,
-        y: from.y,
-        width: Math.floor(from.x - to.x) ? from.x - to.x : 1,
-        height: Math.floor(to.y - from.y) ? to.y - from.y : 1
-      };
-    },
-    TopRight: function(from, to) {
-      return {
-        x: from.x, 
-        y: to.y,
-        width: Math.floor(to.x - from.x) ? to.x - from.x : 1,
-        height: Math.floor(from.y - to.y) ? from.y - to.y : 1
-      };
-    },
-    TopLeft: function(from, to) {
-      return {
-        x: to.x,
-        y: to.y,
-        width: Math.floor(from.x - to.x) ? from.x - to.x : 1,
-        height: Math.floor(from.y - to.y) ? from.y - to.y : 1
-      };
-    }
-  };
 }
