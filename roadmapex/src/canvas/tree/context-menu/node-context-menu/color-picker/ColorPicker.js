fe07e0aef781c8f1ee298101dfc92921a520bc90
billjoynf <45283458+billjoynf@users.noreply.github.com>
2021-03-04 18:59:10 +0200

    feat: custom color field for node

--- roadmapex/src/canvas/tree/context-menu/node-context-menu/color-picker/ColorPicker.js
+++ roadmapex/src/canvas/tree/context-menu/node-context-menu/color-picker/ColorPicker.js
@@ -3,15 +3,35 @@ import './ColorPicker.css';
 import { nodeColors } from '../../../../config.js';
 
 export default class ColorPicker extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      color: this.props.color
+    };
+
+    this.handleColorValue = this.handleColorValue.bind(this);
+  }
+
+  handleColorValue(e) {
+    e.stopPropagation();
+  }
+
   render() {
     if (!this.props.isShown) return null;
 
     return (
       <div className='color-picker'>
-        {nodeColors.map((color, index) => (
-          <div key={index} className='color-picker-item' style={{backgroundColor: color}} 
+        {nodeColors.map((color) => (
+          <div key={color} className='color-picker-item' style={{backgroundColor: color}}
             title={color} data-color={color}></div>
         ))}
+        <span>
+          <input className='color-picker-value'
+                 type="text"
+                 defaultValue={this.props.color}
+                 onClick={this.handleColorValue}
+          />
+        </span>
       </div>    
     )
   }
